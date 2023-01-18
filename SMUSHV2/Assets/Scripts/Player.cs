cfb9869e65a8b1ec4e3733b1921bb0853225e207
OscSanto <108152800+OscSanto@users.noreply.github.com>
2023-01-18 05:04:50 -0600

    Clean Project.
    New Character animations, textures, scripts added. Clean Project created.

---
+++ SMUSHV2/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private float turnSpeed = 90f;
+
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var velocity = Vector3.forward * Input.GetAxis("Vertical") * speed;
+        transform.Translate(velocity * Time.deltaTime);
+        transform.Rotate(Vector3.up, Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed);
+        //we want to have negative values for Speed parameter (to go backwards when the S key is held).
+        //So we change it from velocity.magnitude (which is always positive).
+        animator.SetFloat("Speed", velocity.z);
+    }
+}
